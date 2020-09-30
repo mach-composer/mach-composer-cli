4a6d0e238c52829989ea3423926bb435eb9b7407
Tim Leguijt <t.leguijt@labdigital.nl>
2020-09-30 16:11:31 +0200

    Upgade AWS provider

--- src/mach/templates/partials/aws.tf
+++ src/mach/templates/partials/aws.tf
@@ -1,7 +1,7 @@
 {% set aws = site.aws %}
 provider "aws" {
   region  = "{{ aws.region }}"
-  version = "~> 2.65.0"
+  version = "~> 3.8.0"
 
   assume_role {
     role_arn = "arn:aws:iam::{{ aws.account_id }}:role/{{ aws.deploy_role }}"
@@ -12,7 +12,7 @@ provider "aws" {
 provider "aws" {
   alias   = "{{ provider.name }}"
   region  = "{{ provider.region }}"
-  version = "~> 2.65.0"
+  version = "~> 3.8.0"
 
   assume_role {
     role_arn = "arn:aws:iam::{{ aws.account_id }}:role/{{ aws.deploy_role }}"
