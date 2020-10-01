7b6e9a8734c661d54784a1cf588fd4191689f0c2
Tim Leguijt <t.leguijt@labdigital.nl>
2020-10-01 09:02:38 +0200

    Split azure and aws variables for components

--- src/mach/templates/partials/component.tf
+++ src/mach/templates/partials/component.tf
@@ -4,21 +4,13 @@ module "{{ component.name }}" {
   source            = "{{ definition.source }}{% if definition.use_version_reference %}?ref={{ definition.version }}{% endif %}"
   
   {% if component.is_software_component %}
-  # keep the same order as the module's variables!
+  
+  {% if site.azure %}
+  {% include 'partials/component_azure_variables.tf' %}
+  {% elif site.aws %}
+  {% include 'partials/component_aws_variables.tf' %}
+  {% endif %}
 
-  ### azure related
-  short_name              = "{{ component.short_name }}"
-  name_prefix             = local.name_prefix
-  subscription_id         = local.subscription_id
-  tenant_id               = local.tenant_id
-  service_object_ids      = local.service_object_ids
-  region                  = local.region
-  resource_group_name     = azurerm_resource_group.main.name
-  resource_group_location = azurerm_resource_group.main.location
-  app_service_plan_id     = azurerm_app_service_plan.functionapps.id
-  tags                    = local.tags
-
-  ### functionality related
   component_version       = "{{ definition.version }}"
   environment             = "{{ general_config.environment }}"
   site                    = "{{ site.identifier }}"
@@ -31,14 +23,6 @@ module "{{ component.name }}" {
   sentry_dsn              = "{{ general_config.sentry.dsn }}"
   {% endif %}
 
-  {% if site.aws.api_gateway %}
-  api_gateway = aws_api_gateway_rest_api.main_gateway.arn
-  {% endif %}
-  
-  {% if site.azure.alert_group %}
-  monitor_action_group_id = azurerm_monitor_action_group.alert_action_group.id
-  {% endif %}
-  
   variables = {
     {% for key, value in component.variables.items() %}
     {{ key }} = {{ value|component_value }}
