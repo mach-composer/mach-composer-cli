43457d1ef60466df8aad88bbf8552ad95dc780f3
Tim Leguijt <t.leguijt@labdigital.nl>
2020-11-09 09:51:54 +0100

    Remove unused code

--- src/mach/templates/partials/aws_api_gateway.tf
+++ src/mach/templates/partials/aws_api_gateway.tf
@@ -38,19 +38,8 @@ resource "aws_apigatewayv2_stage" "default" {
   api_id                = aws_apigatewayv2_api.main_gateway.id
   deployment_id         = aws_apigatewayv2_deployment.default.id
 
-  # access_log_settings {
-  #   destination_arn = aws_cloudwatch_log_group.api_gw_primary.arn
-  #   # needs to be one line...
-  #   format          = "{\"requestId\":\"$context.requestId\", \"ip\": \"$context.identity.sourceIp\", \"caller\":\"$context.identity.caller\", \"user\":\"$context.identity.user\", \"requestTime\":\"$context.requestTime\", \"httpMethod\":\"$context.httpMethod\", \"path\":\"$context.path\", \"status\":\"$context.status\", \"protocol\":\"$context.protocol\", \"responseLength\":\"$context.responseLength\"}"
-  # }
-
   depends_on = [aws_apigatewayv2_deployment.default]
 }
-
-# resource "aws_cloudwatch_log_group" "api_gw_primary" {
-#   name              = "{{ site.identifier }}-api-gw-stage-primary-access-logs"
-#   retention_in_days = 30
-# }
 
 resource "aws_apigatewayv2_api_mapping" "custom_domain_mapping" {
   api_id      = aws_apigatewayv2_api.main_gateway.id
