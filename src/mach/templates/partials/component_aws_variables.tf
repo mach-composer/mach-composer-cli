870bba33224e9fc016129dbb46a4c86a1ec06d70
Tim Leguijt <t.leguijt@labdigital.nl>
2020-11-10 09:02:50 +0100

    Removed aws block in general_config
    code_repository setting was not needed

--- src/mach/templates/partials/component_aws_variables.tf
+++ src/mach/templates/partials/component_aws_variables.tf
@@ -2,4 +2,3 @@
 api_gateway = aws_apigatewayv2_api.main_gateway.id
 api_gateway_execution_arn = aws_apigatewayv2_api.main_gateway.execution_arn
 {% endif %}
-code_repository = "{{ site.aws.code_repository }}"
