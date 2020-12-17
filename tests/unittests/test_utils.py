f3698f5e4a61c1041903aecd45e6d5fd8ba8bb9f
Tim Leguijt <t.leguijt@labdigital.nl>
2020-12-17 09:31:33 +0100

    Use tldextract to check on more complex domain names

--- tests/unittests/test_utils.py
+++ tests/unittests/test_utils.py
@@ -6,8 +6,22 @@ from mach import utils
     "url, zone",
     (
         ("https://mach.example.com", "example.com"),
+        ("https://mach.example.co.uk", "example.co.uk"),
         ("api.test.example.com", "test.example.com"),
+        ("api.test.example.co.uk", "test.example.co.uk"),
     ),
 )
 def test_dns_zone_from_url(url, zone):
     assert utils.dns_zone_from_url(url) == zone
+
+
+@pytest.mark.parametrize(
+    "url",
+    (
+        "https://example.com",
+        "https://example.co.uk",
+    ),
+)
+def test_dns_zone_from_url_invalid(url):
+    with pytest.raises(ValueError):
+        utils.dns_zone_from_url(url)
