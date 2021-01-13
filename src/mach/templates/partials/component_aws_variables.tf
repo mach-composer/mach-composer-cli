4ad1a9c66a888dbcdbb2d4b1dd70ff9d60302f89
Tim Leguijt <t.leguijt@labdigital.nl>
2021-01-13 21:28:05 +0100

    Add extra newline between endpoints

--- src/mach/templates/partials/component_aws_variables.tf
+++ src/mach/templates/partials/component_aws_variables.tf
@@ -3,4 +3,5 @@ endpoint_{{ component_endpoint|slugify }} = {
   url = local.endpoint_url_{{ site_endpoint|slugify }}
   api_gateway_id = aws_apigatewayv2_api.{{ site_endpoint|slugify }}_gateway.id
   api_gateway_execution_arn = aws_apigatewayv2_api.{{ site_endpoint|slugify }}_gateway.execution_arn
-}{% endfor %}
+}
+{% endfor %}
