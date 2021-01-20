f14f86e20195a1ec9d4699411ed8e2a676670dcf
Tim Leguijt <t.leguijt@labdigital.nl>
2021-01-20 21:36:43 +0100

    Fix url_locals references for Azure

--- src/mach/templates/partials/endpoints/azure_url_locals.tf
+++ src/mach/templates/partials/endpoints/azure_url_locals.tf
@@ -1,5 +1,6 @@
 locals {
 {% for endpoint in site.used_endpoints %}
-endpoint_url_{{ endpoint.key|slugify }} = "{{ endpoint.url }}"
+endpoint_url_{{ endpoint.key|slugify }} = {% if endpoint.url %}"{{ endpoint.url }}"{% else %}local.frontdoor_domain{% endif %}
 {% endfor %}
+
 }
