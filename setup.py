12e58057c3f24233843de0a984fead37ccd7b0c3
Tim Leguijt <t.leguijt@labdigital.nl>
2021-02-12 22:18:44 +0100

    Read requirements from file

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 import re
 
+import pkg_resources
 from setuptools import find_packages, setup
+
+install_requires = []
+with open("requirements.txt") as r:
+    result = pkg_resources.parse_requirements(r)
+    install_requires = [
+        str(requirement) for requirement in pkg_resources.parse_requirements(r)
+    ]
 
 try:
     with open("README.md", "r") as fh:
@@ -11,42 +19,6 @@ try:
 except IOError:
     readme = ""
 
-install_requires = [
-    "arrow==0.17.0",
-    "attrs==20.3.0",
-    "binaryornot==0.4.4",
-    "certifi==2020.6.20",
-    "chardet==3.0.4",
-    "click==7.1.2",
-    "colorama==0.4.4",
-    "cookiecutter==1.7.2",
-    "dataclasses-json==0.5.2",
-    "dataclasses-jsonschema==2.13.0",
-    "filelock==3.0.12",
-    "idna==2.10",
-    "jinja2-time==0.2.0",
-    "jinja2==2.11.1",
-    "jsonschema==3.2.0",
-    "markupsafe==1.1.1",
-    "marshmallow-enum==1.5.1",
-    "marshmallow==3.8.0",
-    "mypy-extensions==0.4.3",
-    "poyo==0.5.0",
-    "pyrsistent==0.17.3",
-    "python-dateutil==2.8.1",
-    "python-slugify==4.0.1",
-    "pyyaml==5.3",
-    "requests-file==1.5.1",
-    "requests==2.23.0",
-    "six==1.15.0",
-    "stringcase==1.2.0",
-    "text-unidecode==1.3",
-    "tldextract==3.1.0",
-    "typing-extensions==3.7.4.3",
-    "typing-inspect==0.6.0",
-    "urllib3==1.25.10",
-]
-
 setup(
     name="mach-composer",
     version="1.0.0-rc.7",
@@ -55,7 +27,7 @@ setup(
     url="https://github.com/labd/mach-composer",
     description="MACH composer",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     zip_safe=False,
     install_requires=install_requires,
     extras_require={},
@@ -75,7 +47,7 @@ setup(
         "Programming Language :: Python :: Implementation :: CPython",
     ],
     project_urls={
-        'Documentation': 'https://docs.machcomposer.io',
-        'Source': 'https://github.com/labd/mach-composer',
+        "Documentation": "https://docs.machcomposer.io",
+        "Source": "https://github.com/labd/mach-composer",
     },
 )
