449a804a621a1bef4a777348b85fdf9ef60a9932
Tim Leguijt <t.leguijt@labdigital.nl>
2021-02-18 13:22:43 +0100

    Change back reserved setting for service plan

--- src/mach/templates/partials/azure_app_service_plans.tf
+++ src/mach/templates/partials/azure_app_service_plans.tf
@@ -9,7 +9,7 @@ resource "azurerm_app_service_plan" "{{ key|service_plan_resource_name }}" {
   resource_group_name = local.resource_group_name
   location            = local.resource_group_location
   kind                = "{{ plan.kind }}"
-  reserved            = {% if plan.kind|lower == 'linux' %}true{% else %}false{% endif %}
+  reserved            = {% if plan.kind|lower == 'windows' %}false{% else %}true{% endif %}
 
   sku {
     tier = "{{ plan.tier }}"
