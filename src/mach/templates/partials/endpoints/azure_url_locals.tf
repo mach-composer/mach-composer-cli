2e7e0db9ee7c42a053a21f48264ebc242546c118
Tim Leguijt <t.leguijt@labdigital.nl>
2021-03-08 14:28:38 +0100

    Fix azure template spacing

--- src/mach/templates/partials/endpoints/azure_url_locals.tf
+++ src/mach/templates/partials/endpoints/azure_url_locals.tf
@@ -1,8 +1,8 @@
 locals {
 {% for endpoint in site.used_endpoints %}
 endpoint_url_{{ endpoint.key|slugify }} = {% if endpoint.url %}"{{ endpoint.url }}"{% else %}local.frontdoor_domain{% endif %}
-{% endfor %}
 
+{% endfor %}
 }
 
 output "endpoints" {
