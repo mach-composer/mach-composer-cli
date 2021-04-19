289b303ff54c502adaa47ef1fe690d0d6b4320a4
Nayels <groen.nils@gmail.com>
2021-04-19 16:34:37 +0200

    Update main.if

--- examples/gcp/infra/main.tf
+++ examples/gcp/infra/main.tf
@@ -1,15 +1,10 @@
 provider "google" {
-  project = var.project_id
-  region = var.region
+  project     = var.project_id
+  region      = var.region
 }
 
 module "shared_infra" {
-  source                        = "git::https://github.com/labd/terraform-gcp-mach-shared.git"
-  name_prefix                   = "mach-shared-we"
-  region                        = "westeurope"
-  dns_zone_name                 = var.dns_zone_name
-  certificate_access_object_ids = [
-    # Fill in any object IDs of users, groups or service principles that should be able to
-    # access the certificates
-  ]
+  source      = "git::https://github.com/labd/terraform-gcp-mach-shared.git"
+  region      = var.region
+  name_prefix = "mach-playground"
 }
