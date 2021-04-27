abdd81f8340d5ab4964ab89037b3ae17fcf83cd8
Tim Leguijt <t.leguijt@labdigital.nl>
2021-04-27 23:26:04 +0200

    Use api_id as gateway reference

--- src/mach/templates/partials/component_gcp_variables.tf
+++ src/mach/templates/partials/component_gcp_variables.tf
@@ -1,6 +1,6 @@
 {% for component_endpoint, site_endpoint in component.endpoints.items() -%}
 gcp_endpoint_{{ component_endpoint|slugify }} = {
   url = local.endpoint_url_{{ site_endpoint|slugify }}
-  api_id = google_api_gateway_api.{{ site_endpoint|slugify }}_gateway.id
+  api_id = google_api_gateway_api.{{ site_endpoint|slugify }}_gateway.api_id
 }
 {% endfor %}
