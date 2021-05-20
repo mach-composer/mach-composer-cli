0db31fdb9b98450829b8a91bb379d48a0b9d9fcd
Tim Leguijt <t.leguijt@labdigital.nl>
2021-05-20 09:54:00 +0200

    Add tests for component variables

--- tests/unittests/test_templates.py
+++ tests/unittests/test_templates.py
@@ -1,5 +1,5 @@
 import pytest
-from mach import templates
+from mach import exceptions, templates
 
 
 @pytest.mark.parametrize(
@@ -13,3 +13,30 @@ from mach import templates
 )
 def test_zone_name_filter(url, zone):
     assert templates.zone_name(url) == zone
+
+
+def test_render_variable():
+    assert templates.render_variable(True) == "true"
+    assert templates.render_variable(False) == "false"
+
+    assert templates.render_variable(12) == 12
+    assert (
+        templates.render_variable(["value1", False, "value2"])
+        == """["value1",false,"value2"]"""
+    )
+
+    assert (
+        templates.render_variable(r"${component.infra.db_password}")
+        == "module.infra.db_password"
+    )
+
+
+def test_render_config_variable():
+    assert (
+        templates.parse_config_variable(r"${component.infra.db_password}")
+        == "module.infra.db_password"
+    )
+    assert templates.parse_config_variable(r"${component.infra.db_password") == None
+
+    with pytest.raises(exceptions.MachError):
+        assert templates.parse_config_variable(r"${component.infra}")
