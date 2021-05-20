3c3f9976ed87075e5b1fa7867899afcf63002667
Tim Leguijt <t.leguijt@labdigital.nl>
2021-05-20 14:17:46 +0200

    update schema

--- generate_schema.py
+++ generate_schema.py
@@ -3,5 +3,10 @@ import json
 from mach import types
 
 schema = types.MachConfig.json_schema()
+
+del schema["properties"]["output_path"]
+del schema["properties"]["file"]
+del schema["properties"]["file_encrypted"]
+
 with open("schema.json", 'w') as f:
     f.write(json.dumps(schema, indent=2))
