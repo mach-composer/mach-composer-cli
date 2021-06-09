fbcc6d5aeb767acb4a867dcdd77a94efd56a7688
Tim Leguijt <t.leguijt@labdigital.nl>
2021-06-09 17:26:05 +0200

    Ensure unique config/gw ids in order to proper recreate

--- src/mach/templates/partials/endpoints/gcp_api_gateway.tf
+++ src/mach/templates/partials/endpoints/gcp_api_gateway.tf
@@ -14,15 +14,20 @@ data "utils_deep_merge_yaml" "{{ endpoint.key|slugify }}_api_spec" {
   ]
 }
 
+locals {
+  api_spec_{{ endpoint.key|slugify }} = base64encode(data.utils_deep_merge_yaml.{{ endpoint.key|slugify }}_api_spec.output)
+  api_spec_{{ endpoint.key|slugify }}_hash = lower(substr(local.api_spec_{{ endpoint.key|slugify }}, 0, 5))
+}
+
 resource "google_api_gateway_api_config" "{{ endpoint.key|slugify }}" {
   provider = google-beta
   api = google_api_gateway_api.{{ endpoint.key|slugify }}_gateway.api_id
-  api_config_id = "{{ endpoint.key|slugify}}-cfg"
+  api_config_id = "{{ endpoint.key|slugify}}-cfg-${local.api_spec_{{ endpoint.key|slugify }}_hash}"
 
   openapi_documents {
     document {
       path = "spec.yaml"
-      contents = base64encode(data.utils_deep_merge_yaml.{{ endpoint.key|slugify }}_api_spec.output)
+      contents = local.api_spec_{{ endpoint.key|slugify }}
     }
   }
   lifecycle {
@@ -33,12 +38,16 @@ resource "google_api_gateway_api_config" "{{ endpoint.key|slugify }}" {
 resource "google_api_gateway_gateway" "{{ endpoint.key|slugify }}" {
   provider = google-beta
   api_config = google_api_gateway_api_config.{{ endpoint.key|slugify }}.id
-  gateway_id = "{{ endpoint.key|slugify }}-gw"
+  gateway_id = "{{ endpoint.key|slugify }}-gw-${local.api_spec_{{ endpoint.key|slugify }}_hash}"
 
   # TODO: Hard coded for now; we might want to make this configurable in the endpoint settings
   # so we can overwrite for only the API gateway (since for example europe-west3 is not supported 
   # yet)
   region = "europe-west2"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 
