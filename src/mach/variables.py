a201825e9ba7efa76fe392fa5c9fd74d320981a5
Tim Leguijt <t.leguijt@labdigital.nl>
2021-06-09 15:35:31 +0200

    Ignore variable errors when running mach update

--- src/mach/variables.py
+++ src/mach/variables.py
@@ -1,4 +1,8 @@
+from contextlib import contextmanager
+
 from mach.exceptions import MachError
+
+_ignore_var_not_found = False
 
 
 class VariableNotFound(MachError):
@@ -7,7 +11,25 @@ class VariableNotFound(MachError):
 
 
 def resolve_variable(var, variables):
+    try:
+        return _resolve_variable(var, variables)
+    except VariableNotFound:
+        if _ignore_var_not_found:
+            return ""
+        raise
+
+
+def _resolve_variable(var, variables):
     lookup, *remain = var.split(".", maxsplit=1)
+
+    if isinstance(variables, list):
+        try:
+            lookup = int(lookup)
+        except ValueError:
+            raise VariableNotFound("List indicies needs a number to index")
+    elif not isinstance(variables, dict):
+        # We've reached the end-node which is just
+        raise VariableNotFound(var)
 
     try:
         value = variables[lookup]
@@ -16,7 +38,15 @@ def resolve_variable(var, variables):
 
     if remain:
         try:
-            return resolve_variable(remain[0], value)
+            return _resolve_variable(remain[0], value)
         except VariableNotFound:
             raise VariableNotFound(var)
     return value
+
+
+@contextmanager
+def ignore_variable_not_found():
+    global _ignore_var_not_found
+    _ignore_var_not_found = True
+    yield
+    _ignore_var_not_found = False
