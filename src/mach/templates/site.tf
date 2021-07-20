fb7407de395589c88fd9b8c5c925a7e1f1b4cd9b
Tim Leguijt <t.leguijt@labdigital.nl>
2021-07-20 08:49:24 +0200

    Upgrade azure provider to 2.68.0

--- src/mach/templates/site.tf
+++ src/mach/templates/site.tf
@@ -38,7 +38,7 @@ terraform {
 
     {% if site.azure %}
     azurerm = {
-      version = "~> {{ general_config.terraform_config.providers.azure or '2.60.0' }}"
+      version = "~> {{ general_config.terraform_config.providers.azure or '2.68.0' }}"
     }
     {% endif %}
     {% if site.commercetools %}
