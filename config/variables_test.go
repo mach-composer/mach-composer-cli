248e03f788dcdafba117247ffcd381d839342ffc
Michael van Tellingen <m.vantellingen@labdigital.nl>
2022-04-01 10:03:04 +0200

    Don't test encryption for now

--- config/variables_test.go
+++ config/variables_test.go
@@ -18,22 +18,6 @@ func TestVariablesLoad(t *testing.T) {
 		  bar:
 			secrets:
 			  foo: encrypted
-		sops:
-		  kms: []
-		  gcp_kms: []
-		  azure_kv:
-			- vault_url: https://gdsharedwekvsops.vault.azure.net
-			  name: sops-key
-			  version: aa5a053752be4cee83ee0d57200fb1a8
-			  created_at: "2021-06-03T21:15:12Z"
-			  enc: secret
-		  hc_vault: []
-		  age: []
-		  lastmodified: "2022-03-21T10:44:39Z"
-		  mac: mymac
-		  pgp: []
-		  unencrypted_suffix: _unencrypted
-		  version: 3.7.1
 	`)
 
 	utils.AFS.WriteFile("variables.yaml", []byte(content), 0644)
