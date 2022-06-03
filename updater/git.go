622aacce62d0bf0e745aabc3269ed2831d8f9bce
Paul J Stevens <p.stevens@labdigital.nl>
2022-06-03 14:56:53 +0200

    do forced fetch during update
     - prevent bailout due to non-fast-forward updates

--- updater/git.go
+++ updater/git.go
@@ -114,7 +114,7 @@ func fetchGitRepository(ctx context.Context, source *gitSource, cacheDir string)
 		output := runGit(ctx, ".", "clone", "--bare", source.Repository, dest)
 		logrus.Debug(string(output))
 	} else {
-		output := runGit(ctx, dest, "fetch", "origin", "*:*")
+		output := runGit(ctx, dest, "fetch", "-f", "origin", "*:*")
 		logrus.Debug(string(output))
 	}
 }
