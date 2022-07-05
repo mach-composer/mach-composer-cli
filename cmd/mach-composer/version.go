4b42aad776b443e9fba3eae6a05f9b33445b7ac6
Onur Gunduz <o.gunduz@labdigital.nl>
2022-07-05 20:02:44 +0200

    Pass build args to main

--- cmd/mach-composer/version.go
+++ cmd/mach-composer/version.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-	"github.com/labd/mach-composer/utils"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Return version information of the mach-composer cli",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Printf("mach-composer %s (%s) - %s\n", utils.Version, utils.Commit[:7], utils.Date)
+		fmt.Printf("mach-composer %s (%s) - %s\n", version, commit[:7], date)
 		return nil
 	},
 }
