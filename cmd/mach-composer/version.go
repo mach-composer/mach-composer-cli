27402667da1193b2dec67e7f329ebf6ee87a962b
Onur Gunduz <o.gunduz@labdigital.nl>
2022-07-05 20:03:18 +0200

    Show only version in version output

--- cmd/mach-composer/version.go
+++ cmd/mach-composer/version.go
@@ -10,7 +10,14 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Return version information of the mach-composer cli",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		fmt.Printf("mach-composer %s (%s) - %s\n", version, commit[:7], date)
+		if commit != "" && date != "" {
+			fmt.Printf("v%s (%s) - %s\n", version, commit[:7], date)
+		} else {
+			if version != "development" {
+				version = fmt.Sprintf("v%s", version)
+			}
+			fmt.Println(version)
+		}
 		return nil
 	},
 }
