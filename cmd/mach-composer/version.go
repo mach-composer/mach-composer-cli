30c4472f1342b7db58f210ca286ef1157a3356bf
Onur Gunduz <o.gunduz@labdigital.nl>
2022-07-08 02:35:56 +0200

    Revert version cmd change

--- cmd/mach-composer/version.go
+++ cmd/mach-composer/version.go
@@ -10,14 +10,7 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Return version information of the mach-composer cli",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if commit != "" && date != "" {
-			fmt.Printf("v%s (%s) - %s\n", version, commit[:7], date)
-		} else {
-			if version != "development" {
-				version = fmt.Sprintf("v%s", version)
-			}
-			fmt.Println(version)
-		}
+		fmt.Printf("mach-composer %s (%s) - %s\n", version, commit[:7], date)
 		return nil
 	},
 }
