7274119efbec77e36df8718a2622529f4e155469
Onur Gunduz <o.gunduz@labdigital.nl>
2022-07-21 16:59:05 +0200

    Remove redundant functions

--- cmd/mach-composer/components.go
+++ cmd/mach-composer/components.go
@@ -13,28 +13,21 @@ var componentsCmd = &cobra.Command{
 		preprocessGenerateFlags()
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if err := componentsFunc(args); err != nil {
-			return err
+		cfg := LoadConfig()
+		generateFlags.ValidateSite(cfg)
+
+		fmt.Printf("%s:\n", generateFlags.configFile)
+		for _, component := range cfg.Components {
+			fmt.Printf(" - %s\n", component.Name)
+			fmt.Printf("   version: %s\n", component.Version)
 		}
+
+		fmt.Println("")
+
 		return nil
 	},
 }
 
 func init() {
 	registerGenerateFlags(componentsCmd)
-}
-
-func componentsFunc(args []string) error {
-	cfg := LoadConfig()
-	generateFlags.ValidateSite(cfg)
-
-	fmt.Printf("%s:\n", generateFlags.configFile)
-	for _, component := range cfg.Components {
-		fmt.Printf(" - %s\n", component.Name)
-		fmt.Printf("   version: %s\n", component.Version)
-	}
-
-	fmt.Println("")
-
-	return nil
 }
