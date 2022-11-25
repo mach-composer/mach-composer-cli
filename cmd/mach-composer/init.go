abb74fe4e1d54c64c06502281a2646112daa3e5e
Tim Leguijt <t.leguijt@labdigital.nl>
2022-11-25 20:14:32 +0100

    implement init command

--- cmd/mach-composer/init.go
+++ cmd/mach-composer/init.go
@@ -1,14 +1,42 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+
+	"github.com/labd/mach-composer/internal/generator"
+	"github.com/labd/mach-composer/internal/runner"
+	"github.com/spf13/cobra"
+)
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize site directories Terraform files.",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		// if err := someFunc(); err != nil {
-		// 	return err
-		// }
-		return nil
+	PreRun: func(cmd *cobra.Command, args []string) {
+		preprocessGenerateFlags()
 	},
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return handleError(initFunc(cmd.Context(), args))
+	},
+}
+
+func init() {
+	registerGenerateFlags(initCmd)
+}
+
+func initFunc(ctx context.Context, args []string) error {
+	cfg := LoadConfig(ctx)
+	generateFlags.ValidateSite(cfg)
+
+	paths, err := generator.WriteFiles(cfg, &generator.GenerateOptions{
+		OutputPath: generateFlags.outputPath,
+		Site:       generateFlags.siteName,
+	})
+	if err != nil {
+		return err
+	}
+
+	return runner.TerraformInit(ctx, cfg, paths, &runner.InitOptions{
+		Site: generateFlags.siteName,
+	})
 }
