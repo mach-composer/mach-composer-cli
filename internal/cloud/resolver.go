665412c6d819a3e6eaf567dc16ee2a5f1bd8620c
Michael van Tellingen <michaelvantellingen@gmail.com>
2023-03-09 17:11:43 +0100

    Only create mach composer client when needed
    Lazy init the client instead

--- internal/cloud/resolver.go
+++ internal/cloud/resolver.go
@@ -7,14 +7,6 @@ import (
 )
 
 func ResolveComponentsData(ctx context.Context, cfg *config.MachConfig) error {
-	if cfg.MachComposer.Cloud.Client == nil {
-		client, err := NewClient(ctx)
-		if err != nil {
-			return err
-		}
-		cfg.MachComposer.Cloud.Client = client
-	}
-
 	for i := range cfg.Components {
 		if err := resolveComponentVersion(ctx, cfg, &cfg.Components[i]); err != nil {
 			return err
@@ -26,6 +18,14 @@ func ResolveComponentsData(ctx context.Context, cfg *config.MachConfig) error {
 func resolveComponentVersion(ctx context.Context, cfg *config.MachConfig, c *config.Component) error {
 	if c.Version != "$LATEST" {
 		return nil
+	}
+
+	if cfg.MachComposer.Cloud.Client == nil {
+		client, err := NewClient(ctx)
+		if err != nil {
+			return err
+		}
+		cfg.MachComposer.Cloud.Client = client
 	}
 
 	client := cfg.MachComposer.Cloud.Client
