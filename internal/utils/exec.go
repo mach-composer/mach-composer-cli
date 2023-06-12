6a31caa8a41e2ad6e5e745c92e249a1461bd9d9f
Michael van Tellingen <michaelvantellingen@gmail.com>
2023-06-12 22:51:22 +0200

    Improve signal handling of child processes
    When sending a SIGTERM to mach composer it should nicely shutdown
    terraform by forwarding the SIGTERM and waiting for a few seconds

--- internal/utils/exec.go
+++ internal/utils/exec.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/rs/zerolog/log"
 )
@@ -30,9 +31,54 @@ func RunInteractive(ctx context.Context, command string, cwd string, args ...str
 		return err
 	}
 
-	err = cmd.Wait()
-	if err != nil {
-		return fmt.Errorf("command exited: %s %s (in %s)", command, strings.Join(args, " "), cwd)
+	// Wait for the command to complete or the context to be cancelled
+	done := make(chan error)
+	go func() {
+		done <- cmd.Wait()
+	}()
+
+	select {
+	case <-ctx.Done():
+		return StopProcess(cmd)
+
+	case err := <-done:
+		if err != nil {
+			return fmt.Errorf("command (%s) failed: %w (args: %s , cwd: %s)", command, err, strings.Join(args, " "), cwd)
+		}
 	}
+
 	return nil
+}
+
+func StopProcess(cmd *exec.Cmd) error {
+	fmt.Println("Context cancelled, waiting for the command to exit...")
+	err := cmd.Process.Signal(os.Interrupt)
+	if err != nil {
+		return fmt.Errorf("failed to send interrupt signal: %w\n", err)
+	}
+
+	// Poll for the process to exit
+	pollInterval := 250 * time.Millisecond
+	ticker := time.NewTicker(pollInterval)
+	current := time.Now()
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("Waiting...")
+			fmt.Println(cmd.ProcessState)
+			if cmd.ProcessState != nil && cmd.ProcessState.Exited() {
+				fmt.Println("Command exited.")
+				return nil
+			}
+			if current.Add(10 * time.Second).After(time.Now()) {
+				err := cmd.Process.Signal(os.Kill)
+				if err != nil {
+					return fmt.Errorf("failed to send interrupt signal: %w\n", err)
+				}
+				return fmt.Errorf("command did not exit within the specified duration, killing")
+			}
+		}
+	}
 }
