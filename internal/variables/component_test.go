c3c10ea1f58488d3269033e33190e88ea2688fd4
Thomas De Meyer <t.demeyer@labdigital.nl>
2023-08-18 15:18:33 +0200

    added possibility to interpolate slices

--- internal/variables/component_test.go
+++ internal/variables/component_test.go
@@ -26,24 +26,90 @@ func TestParseTemplateVariable(t *testing.T) {
 	}
 }
 
-func TestInterpolateComponentVars(t *testing.T) {
-	data := map[string]any{
-		"my-simple-case": "${component.foobar.var}",
-		"my-string-case": "-> ${component.foobar.var} <-",
-		"my-nested-case": map[string]any{
-			"my-simple-case": "${component.foobar.var}",
-			"my-string-case": "-> ${component.foobar.var} - ${component.foobar.bar}<-",
+func TestInterpolateComponentVarsSuccess(t *testing.T) {
+	var testCases = []struct {
+		Description string
+		Input       map[string]any
+		Expected    map[string]any
+	}{
+		{
+			Description: "Simple test case",
+			Input:       map[string]any{"key": "${component.foobar.var}"},
+			Expected:    map[string]any{"key": "${module.foobar.var}"},
+		},
+		{
+			Description: "String test case",
+			Input:       map[string]any{"key": "-> ${component.foobar.var} <-"},
+			Expected:    map[string]any{"key": "-> ${module.foobar.var} <-"},
+		},
+		{
+			Description: "Integer test case",
+			Input:       map[string]any{"key": 123},
+			Expected:    map[string]any{"key": 123},
+		},
+		{
+			Description: "Boolean test case",
+			Input:       map[string]any{"key": true},
+			Expected:    map[string]any{"key": true},
+		},
+		{
+			Description: "Float test case",
+			Input:       map[string]any{"key": 1.1},
+			Expected:    map[string]any{"key": 1.1},
+		},
+		{
+			Description: "Nested map test case",
+			Input: map[string]any{
+				"key": map[string]any{
+					"key-1": "${component.foobar.var}",
+					"key-2": "-> ${component.foobar.var} - ${component.foobar.bar}<-",
+				}},
+			Expected: map[string]any{
+				"key": map[string]any{
+					"key-1": "${module.foobar.var}",
+					"key-2": "-> ${module.foobar.var} - ${module.foobar.bar}<-",
+				}},
+		},
+		{
+			Description: "Nested slice test case",
+			Input: map[string]any{
+				"key": []interface{}{
+					`${component.site-topgeschenken.service_link_map["topbloemen-nl"]}`,
+					`${component.site-topgeschenken.service_link_map["topbloemen-be"]}`,
+				},
+			},
+			Expected: map[string]any{
+				"key": []interface{}{
+					`${module.site-topgeschenken.service_link_map["topbloemen-nl"]}`,
+					`${module.site-topgeschenken.service_link_map["topbloemen-be"]}`,
+				}},
+		},
+		{
+			Description: "Slice with nested map test case",
+			Input: map[string]any{
+				"key": []interface{}{
+					map[string]any{
+						"key": `${component.site-topgeschenken.service_link_map["topbloemen-nl"]}`,
+					},
+					map[string]any{
+						"key": `${component.site-topgeschenken.service_link_map["topbloemen-be"]}`,
+					},
+				},
+			},
+			Expected: map[string]any{
+				"key": []interface{}{
+					map[string]any{"key": `${module.site-topgeschenken.service_link_map["topbloemen-nl"]}`},
+					map[string]any{"key": `${module.site-topgeschenken.service_link_map["topbloemen-be"]}`},
+				},
+			},
 		},
 	}
-	expected := map[string]any{
-		"my-simple-case": "${module.foobar.var}",
-		"my-string-case": "-> ${module.foobar.var} <-",
-		"my-nested-case": map[string]any{
-			"my-simple-case": "${module.foobar.var}",
-			"my-string-case": "-> ${module.foobar.var} - ${module.foobar.bar}<-",
-		},
+
+	for _, testCase := range testCases {
+		t.Run(testCase.Description, func(t *testing.T) {
+			result, err := InterpolateComponentVars(testCase.Input)
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.Expected, result)
+		})
 	}
-	result, err := InterpolateComponentVars(data)
-	assert.NoError(t, err)
-	assert.Equal(t, expected, result)
 }
