691b861ea06998fda01b01236e1e617e72633abe
Thomas De Meyer <t.demeyer@labdigital.nl>
2023-10-23 13:13:48 +0200

    feat: added changie file

--- internal/config/hash.go
+++ internal/config/hash.go
@@ -4,17 +4,16 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-	"github.com/mach-composer/mach-composer-cli/internal/variables"
 )
 
 func ComputeHash(cfg *MachConfig) (string, error) {
 	hashConfig := struct {
-		MachComposer MachComposer         `json:"mach_composer"`
-		Global       GlobalConfig         `json:"global"`
-		Sites        []SiteConfig         `json:"sites"`
-		Components   []ComponentConfig    `json:"components"`
-		Filename     string               `json:"filename"`
-		Variables    *variables.Variables `json:"variables"`
+		MachComposer MachComposer      `json:"mach_composer"`
+		Global       GlobalConfig      `json:"global"`
+		Sites        []SiteConfig      `json:"sites"`
+		Components   []ComponentConfig `json:"components"`
+		Filename     string            `json:"filename"`
+		Variables    *Variables        `json:"variables"`
 	}{
 		MachComposer: cfg.MachComposer,
 		Global:       cfg.Global,
