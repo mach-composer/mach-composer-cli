8d3874fc910f0931a30e1817dd1e7d9817ef5a5e
Thomas De Meyer <t.demeyer@labdigital.nl>
2024-02-23 11:02:06 +0100

    fix: made hash check more permissive for site components

--- internal/graph/site.go
+++ internal/graph/site.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"errors"
 	"github.com/dominikbraun/graph"
 	"github.com/mach-composer/mach-composer-cli/internal/config"
 	"github.com/mach-composer/mach-composer-cli/internal/utils"
@@ -47,8 +48,15 @@ func (s *Site) HasChanges() (bool, error) {
 
 	tfHash, err := utils.ParseHashOutput(s.outputs)
 	if err != nil {
-		log.Warn().Msgf("Could not parse hash output: %s", err)
-		return true, nil
+		var serr *utils.MissingHashError
+		if errors.As(err, &serr) {
+			log.Warn().Msgf("Could not parse hash output: %s. This is "+
+				"generally caused by incorrect output state, but will be updated "+
+				"at the next succesful update, so can be ignored", serr)
+			return true, nil
+		}
+
+		return false, err
 	}
 
 	return hash != tfHash, nil
--- internal/graph/site_component_test.go
+++ internal/graph/site_component_test.go
@@ -18,6 +18,31 @@ func TestSiteComponent_Hash_Ok(t *testing.T) {
 	assert.Equal(t, "e95a356ad37de77080ea6827f0e29e56aa2dbc58088b955861ae18b1eef841a0", hash, "Hashes should be equal")
 }
 
+func TestSiteComponent_HasChanges_HashNotFound(t *testing.T) {
+	su := NewSiteComponent(nil, "", "", "", nil,
+		config.SiteConfig{},
+		config.SiteComponentConfig{Name: "b", Definition: &config.ComponentConfig{Name: "b"}},
+	)
+	su.outputs = cty.ObjectVal(map[string]cty.Value{})
+
+	changed, err := su.HasChanges()
+	assert.NoError(t, err)
+	assert.True(t, changed)
+}
+
+func TestSiteComponent_HasChanges_Error(t *testing.T) {
+	su := NewSiteComponent(nil, "", "", "", nil,
+		config.SiteConfig{},
+		config.SiteComponentConfig{Name: "b", Definition: &config.ComponentConfig{Name: "b"}},
+	)
+	su.outputs = cty.ObjectVal(map[string]cty.Value{
+		"hash": cty.StringVal("some-hash"),
+	})
+
+	_, err := su.HasChanges()
+	assert.Error(t, err)
+}
+
 func TestSiteComponent_HasChanges_True(t *testing.T) {
 	su := NewSiteComponent(nil, "", "", "", nil,
 		config.SiteConfig{},
